d3e55749909132abca6e3c9013d562af043c72d1
Pezzini S <susanna.pezzini@gmail.com>
2019-05-15 14:49:30 +0700

    add edited rout for patch location

--- controllers/user_update.js
+++ controllers/user_update.js
@@ -4,7 +4,10 @@ const db_user = require('../models/user')
 module.exports = function(req, res, next) {
 	db_user.findByIdAndUpdate(
 		req.user._id,
-		req.body)
+		req.body, 
+		{
+			new: true
+		})
         .then(function(location_updated) {
 			res.status(200).json(location_updated)
 			console.log('user', req.user._id)
