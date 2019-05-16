ec3a8a06625b073212e3aa7704555f351338a033
rjustin <ryan.justin@ymail.com>
2019-05-16 23:31:20 +0800

    Fix the user location API to exlude self and add hasWorkout field

--- controllers/users_location_get.js
+++ controllers/users_location_get.js
@@ -1,7 +1,11 @@
 // Import db_user
 const db_user = require('../models/user')
+const mongoose = require('mongoose')
 
 module.exports = function(req, res, next) {
+
+    let userid = req.user._id;
+    let id = mongoose.Types.ObjectId(userid);
 
     db_user.aggregate([
         {
@@ -18,12 +22,19 @@ module.exports = function(req, res, next) {
             },
         },
         {
+            $match: { _id: {$ne: id} }
+        },
+        {
             $project:
             {
                 _id: 1,
                 name: 1,
                 email: 1,
-                distance: 1
+                distance: 1,
+                hasWorkout:
+                {
+                    $cond: { if: { "$gt": [ { "$size": "$workout" }, 0 ] }, then: true, else: false }
+                }
             }
         }
     ]).then(function(users) {
