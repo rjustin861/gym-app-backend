337d921c8ccda2b7f6c407d5e89c390737497bbc
rjustin <ryan.justin@ymail.com>
2019-05-21 17:11:50 +0800

    Convert the datetime to utc before saving to DB

--- controllers/workout_post.js
+++ controllers/workout_post.js
@@ -1,5 +1,6 @@
 // Import db_user
 const db_user = require('../models/user')
+const moment = require('moment')
 
 module.exports = function(req, res, next) {
 
@@ -7,7 +8,8 @@ module.exports = function(req, res, next) {
     
     let userid = req.user._id
     let workout = req.body.workout
-    console.log('workout', workout)
+    workout.start_date = moment(workout['start_date']).utc()
+    workout.end_date = moment(workout['end_date']).utc()
     
     db_user.findOneAndUpdate( {_id:userid}, {$push: {workout:workout}}, {new: true})
         .then(function(new_workout) {
